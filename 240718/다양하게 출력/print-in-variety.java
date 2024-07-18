c8b21dd7097c2ff0534d94d734ca10a938e9bae8
노원희 (Wonhui Roh / Neo) <87654809+neoseurae12@users.noreply.github.com>
2024-07-18 23:54:38 +0900

    Title: 다양하게 출력, Time: 93ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240718/다양하게 출력/print-in-variety.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Total days in Year");
+        System.out.println(365);
+        System.out.print("Circumference rate\n");
+        System.out.print(3.1415926535);
+    }
+}
