c7f04d5cb8b355460cc1722d4b0c627eea530b38
노원희 (Wonhui Roh / Neo) <87654809+neoseurae12@users.noreply.github.com>
2024-07-19 17:30:23 +0900

    Title: 데이터 교환, Time: 93ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240719/데이터 교환/exchange-data.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5, b = 6, c = 7;
+        int temp = a;
+        a = c;
+        c = b;
+        b = temp;
+
+        System.out.print(a + "\n" + b + "\n" + c);
+    }
+}
