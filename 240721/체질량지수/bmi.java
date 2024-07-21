1764657ae2581cefcf4e03a9481895ccc281a8cd
노원희 (Wonhui Roh / Neo) <87654809+neoseurae12@users.noreply.github.com>
2024-07-21 19:26:44 +0900

    Title: 체질량지수, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240721/체질량지수/bmi.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int h = sc.nextInt(), w = sc.nextInt();
+
+        double b = (double)(10000 * w) / (h * h);
+
+        System.out.println((int)b);
+
+        if (b >= 25) {
+            System.out.println("Obesity");
+        }
+    }
+}
