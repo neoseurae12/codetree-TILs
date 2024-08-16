45e2926d2c5ace32a3029a6bc1266d1f92082c9b
노원희 (Wonhui Roh / Neo) <87654809+neoseurae12@users.noreply.github.com>
2024-08-16 18:27:24 +0900

    Title: 4번의 크기 비교, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

--- 240816/4번의 크기 비교/4-time-comparison.java
+++ 240816/4번의 크기 비교/4-time-comparison.java
@@ -1,12 +1,6 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int compare(int a, int x) {
-        if (a > x)
-            return 1;
-        else
-            return 0;
-    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -17,9 +11,9 @@ public class Main {
         int d = sc.nextInt();
         int e = sc.nextInt();
 
-        System.out.println(compare(a, b));
-        System.out.println(compare(a, c));
-        System.out.println(compare(a, d));
-        System.out.println(compare(a, e));
+        System.out.println(a > b ? 1 : 0);
+        System.out.println(a > c ? 1 : 0);
+        System.out.println(a > d ? 1 : 0);
+        System.out.println(a > e ? 1 : 0);
     }
 }
