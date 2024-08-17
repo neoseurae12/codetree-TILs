0dec0a7d4675d6047c8e4f68f7001bc1ab5b8399
노원희 (Wonhui Roh / Neo) <87654809+neoseurae12@users.noreply.github.com>
2024-08-17 23:22:00 +0900

    Title: 홀수만 출력, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240817/홀수만 출력/output-only-odd.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 변수 선언
+        int a, b;
+
+        // 입력
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        // 출력
+        for (int i = a; i <= b; i += 2) {
+            System.out.print(i + " ");
+        }
+    }
+}
