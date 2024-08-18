41301a66c8cb7f08b0b4405f767ece2a7a7234bb
노원희 (Wonhui Roh / Neo) <87654809+neoseurae12@users.noreply.github.com>
2024-08-18 15:57:38 +0900

    Title: k번째로 신기한 문자열, Time: 148ms, Memory: 10MB, Status: Passed - CodeTree

--- 240818/k번째로 신기한 문자열/kth-special-string.java
+++ 240818/k번째로 신기한 문자열/kth-special-string.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,21 +7,23 @@ public class Main {
 
         int n, k;
         String prefix;
-        ArrayList<String> wordsWithPrefix = new ArrayList<>();
-
+        
         n = sc.nextInt();
         k = sc.nextInt();
         prefix = sc.next();
 
+        String[] wordsWithPrefix = new String[n];
+        int idx = 0;
         for (int i = 0; i < n; i++) {
             String word = sc.next();
 
-            if (word.startsWith(prefix))
-                wordsWithPrefix.add(word);
+            if (word.startsWith(prefix)) {
+                wordsWithPrefix[idx++] = word;
+            }
         }
 
-        Collections.sort(wordsWithPrefix);
+        Arrays.sort(wordsWithPrefix, 0, idx);
 
-        System.out.println(wordsWithPrefix.get(k - 1));
+        System.out.println(wordsWithPrefix[k - 1]);
     }
 }
