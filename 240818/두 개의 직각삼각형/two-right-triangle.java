85f026ef1a229ef0c35e86b488e1dd9710f814e1
노원희 (Wonhui Roh / Neo) <87654809+neoseurae12@users.noreply.github.com>
2024-08-18 21:42:29 +0900

    Title: 두 개의 직각삼각형, Time: 167ms, Memory: 10MB, Status: Passed - CodeTree

--- 240818/두 개의 직각삼각형/two-right-triangle.java
+++ 240818/두 개의 직각삼각형/two-right-triangle.java
@@ -7,7 +7,7 @@ public class Main {
     }
 
     public static void drawBlank(int n, int i) {
-        for (int j = 0; j < i; j++)
+        for (int j = 0; j < i * 2; j++)
             System.out.print(" ");
     }
 
@@ -18,7 +18,6 @@ public class Main {
 
         for (int i = 0; i < n; i++) {
             drawStar(n, i);
-            drawBlank(n, i);
             drawBlank(n, i);
             drawStar(n, i);
             System.out.println();
