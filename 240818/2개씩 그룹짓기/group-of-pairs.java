040a4dce9ea3dd55795dc69d2b028d882cfc7a88
노원희 (Wonhui Roh / Neo) <87654809+neoseurae12@users.noreply.github.com>
2024-08-18 15:26:09 +0900

    Title: 2개씩 그룹짓기, Time: 217ms, Memory: 13MB, Status: Passed - CodeTree

--- 240818/2개씩 그룹짓기/group-of-pairs.java
+++ 240818/2개씩 그룹짓기/group-of-pairs.java
@@ -8,13 +8,13 @@ public class Main {
         int N;
         N = sc.nextInt();
 
-        int maxSum = 0;
         int[] nums = new int[2 * N];
         for (int i = 0; i < 2 * N; i++)
             nums[i] = sc.nextInt();
 
         Arrays.sort(nums);
 
+        int maxSum = 0;
         for (int i = 0; i < N; i++) {
             int sum = nums[i] + nums[2 * N - 1 - i];
             if (sum > maxSum)
