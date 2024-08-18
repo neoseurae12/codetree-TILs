38ce54a520e68c5b696ff54eb5af2f5502dacbd6
노원희 (Wonhui Roh / Neo) <87654809+neoseurae12@users.noreply.github.com>
2024-08-18 20:50:40 +0900

    Title: 정사각형 두 개 출력, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240818/정사각형 두 개 출력/output-two-rectangles.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void drawRectangle(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++)
+                System.out.print("*");
+            
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        drawRectangle(n);
+        drawRectangle(n);
+    }
+}
