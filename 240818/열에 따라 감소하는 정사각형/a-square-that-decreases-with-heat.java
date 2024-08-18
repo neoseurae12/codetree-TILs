e682bcff7691c096d2bab5a9b566b28dc625ad13
노원희 (Wonhui Roh / Neo) <87654809+neoseurae12@users.noreply.github.com>
2024-08-18 23:34:59 +0900

    Title: 열에 따라 감소하는 정사각형, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

--- 240818/열에 따라 감소하는 정사각형/a-square-that-decreases-with-heat.java
+++ 240818/열에 따라 감소하는 정사각형/a-square-that-decreases-with-heat.java
@@ -8,7 +8,7 @@ public class Main {
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++)
-                System.out.print(n-j + " ");
+                System.out.print((n - j) + " ");
             System.out.println();
         }
     }
