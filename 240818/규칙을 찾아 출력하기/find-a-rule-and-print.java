e551f6268ccea323c4a8017bec14e5ac660b342b
노원희 (Wonhui Roh / Neo) <87654809+neoseurae12@users.noreply.github.com>
2024-08-18 23:27:10 +0900

    Title: 규칙을 찾아 출력하기, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

--- 240818/규칙을 찾아 출력하기/find-a-rule-and-print.java
+++ 240818/규칙을 찾아 출력하기/find-a-rule-and-print.java
@@ -13,7 +13,7 @@ public class Main {
         for (int i = 1; i < n; i++) {
             for (int j = 0; j < i; j++)
                 System.out.print("* ");
-            for (int j = 0; j < n - i; j++)
+            for (int j = 0; j < n - i - 1; j++)
                 System.out.print("  ");
             System.out.println("*");
         }
