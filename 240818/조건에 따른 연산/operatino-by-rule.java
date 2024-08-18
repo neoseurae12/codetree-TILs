b37e415a7c62a00cd6176fad31fa1ae8ea64e42f
노원희 (Wonhui Roh / Neo) <87654809+neoseurae12@users.noreply.github.com>
2024-08-18 20:31:02 +0900

    Title: 조건에 따른 연산, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240818/조건에 따른 연산/operatino-by-rule.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n, cnt = 0;
+        n = sc.nextInt();
+
+        while (n < 1000) {
+            if (n % 2 == 0)
+                n = n * 3 + 1;
+            else
+                n = n * 2 + 2;
+
+            cnt++;
+        }
+
+        System.out.println(cnt);
+    }
+}
