d92919975e65edcb12b61e7d60c00eea7f43e22b
노원희 (Wonhui Roh / Neo) <87654809+neoseurae12@users.noreply.github.com>
2024-08-19 23:57:12 +0900

    Title: 행에 따라 다르게 출력, Time: 241ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240819/행에 따라 다르게 출력/output-differently-for-row.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int cnt = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i % 2 == 1) { // 홀수 번째 줄
+                    cnt++;
+                    System.out.print(cnt + " ");
+                } else { // 짝수 번째 줄
+                    cnt += 2;
+                    System.out.print(cnt + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
