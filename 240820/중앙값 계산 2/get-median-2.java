6b1884490393f4a074c3dc1177ecc8df479ffab3
노원희 (Wonhui Roh / Neo) <87654809+neoseurae12@users.noreply.github.com>
2024-08-20 16:21:04 +0900

    Title: 중앙값 계산 2, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 240820/중앙값 계산 2/get-median-2.java
+++ 240820/중앙값 계산 2/get-median-2.java
@@ -12,7 +12,7 @@ public class Main {
             nums[i] = sc.nextInt();
 
             if ((i + 1) % 2 != 0) { // 홀수 번째일 경우
-                Arrays.sort(nums, 0, i + 1);
+                Arrays.sort(nums, 0, i + 1);    // 주의할 점: Arrays.sort() 함수의 범위 지정
                 System.out.print(nums[i / 2] + " ");
             }
         }
