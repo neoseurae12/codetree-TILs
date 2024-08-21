c892c5be5d5e2fa1392d72a29ddf4c9927577924
노원희 (Wonhui Roh / Neo) <87654809+neoseurae12@users.noreply.github.com>
2024-08-21 17:59:46 +0900

    Title: Next Level, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240821/Next Level/next-level.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class User {
+    String id;
+    int level;
+
+    public User(String id, int level) {
+        this.id = id;
+        this.level = level;
+    }
+
+    public User() {
+        this("", 0);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        User u1 = new User("codetree", 10);
+        User u2 = new User();
+
+        u2.id = sc.next();
+        u2.level = sc.nextInt();
+
+        System.out.printf("user %s lv %d\n", u1.id, u1.level);
+        System.out.printf("user %s lv %d\n", u2.id, u2.level);
+    }
+}
