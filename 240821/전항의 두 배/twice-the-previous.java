b02a0a45e138057ce1c342081eebcdabec7fa6d8
노원희 (Wonhui Roh / Neo) <87654809+neoseurae12@users.noreply.github.com>
2024-08-21 14:00:31 +0900

    Title: 전항의 두 배, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 240821/전항의 두 배/twice-the-previous.java
+++ 240821/전항의 두 배/twice-the-previous.java
@@ -5,19 +5,17 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int A1, A2;
-
         A1 = sc.nextInt();
         A2 = sc.nextInt();
 
-        System.out.print(A1 + " ");
-        System.out.print(A2 + " ");
+        int[] arr = new int[10];
+        arr[0] = A1;
+        arr[1] = A2;
 
-        for (int i = 2; i < 10; i++) {
-            int An = A2 + 2 * A1;
-            System.out.print(An + " ");
-            
-            A1 = A2;
-            A2 = An;
-        }
+        for (int i = 2; i < 10; i++)
+            arr[i] = arr[i - 1] + 2 * arr[i - 2];
+
+        for (int i = 0; i < 10; i++)
+            System.out.print(arr[i] + " ");
     }
 }
