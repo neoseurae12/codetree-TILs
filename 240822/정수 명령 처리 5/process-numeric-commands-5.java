f3ff87b2c2ea150348b25c0f205cc145470a9065
노원희 (Wonhui Roh / Neo) <87654809+neoseurae12@users.noreply.github.com>
2024-08-22 17:20:02 +0900

    Title: 정수 명령 처리 5, Time: 443ms, Memory: 24MB, Status: Passed - CodeTree

--- 240822/정수 명령 처리 5/process-numeric-commands-5.java
+++ 240822/정수 명령 처리 5/process-numeric-commands-5.java
@@ -2,29 +2,23 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Main {
-    public static ArrayList<Integer> darr = new ArrayList<>();
-
-    public static void pushBack(int a) {
-        darr.add(a);
-    }
-
-    public static void popBack() {
-        darr.remove(darr.size() - 1);
-    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
 
+        // 동적 배열
+        ArrayList<Integer> darr = new ArrayList<>();
+
         for (int i = 0; i < n; i++) {
             String command = sc.next();
             
             if (command.equals("push_back")) {
                 int num = sc.nextInt();
-                pushBack(num);
+                darr.add(num);
             } else if (command.equals("pop_back")) {
-                popBack();
+                darr.remove(darr.size() - 1);
             } else if (command.equals("size")) {
                 System.out.println(darr.size());
             } else {    // "get"
