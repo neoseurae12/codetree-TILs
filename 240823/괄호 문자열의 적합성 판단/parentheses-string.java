17418fbe14460d793f3ac60bc667752a7f81d4b8
노원희 (Wonhui Roh / Neo) <87654809+neoseurae12@users.noreply.github.com>
2024-08-23 12:53:08 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 102ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 240823/괄호 문자열의 적합성 판단/parentheses-string.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.Deque;
+import java.util.ArrayDeque;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for (char c : str.toCharArray()) {
+            if (c == '(') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    System.out.println("No");
+                    break;
+                }
+
+                stack.pop();
+            }
+        }
+
+        if (stack.isEmpty())
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+    }
+}
