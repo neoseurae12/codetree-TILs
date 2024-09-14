ff9927603da0a7e87de1c338fdf041a75a7f741b
노원희 (Wonhui Roh / Neo) <87654809+neoseurae12@users.noreply.github.com>
2024-09-14 19:00:20 +0900

    Title: 비오는 날, Time: 97ms, Memory: 9MB, Status: Passed - CodeTree

--- 240914/비오는 날/rainy-day.java
+++ 240914/비오는 날/rainy-day.java
@@ -21,25 +21,23 @@ public class Main {
         StringTokenizer st;
 
         int n = Integer.parseInt(br.readLine());
-        WeatherDatum[] weatherData = new WeatherDatum[n];
+        
+        WeatherDatum nearestRainyDatum = null;
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             String date = st.nextToken();
             String day = st.nextToken();
             String weather = st.nextToken();
-            weatherData[i] = new WeatherDatum(date, day, weather);
-        }
+            WeatherDatum wd = new WeatherDatum(date, day, weather);
 
-        WeatherDatum nearestRainyDatum = null;
-        for (int i = 0; i < n; i++) {
-            if (weatherData[i].weather.equals("Rain")) {
+            if (wd.weather.equals("Rain")) {
                 if (nearestRainyDatum == null) {
-                    nearestRainyDatum = weatherData[i];
+                    nearestRainyDatum = wd;
                     continue;
                 }
 
-                if (weatherData[i].date.compareTo(nearestRainyDatum.date) < 0) {
-                    nearestRainyDatum = weatherData[i];
+                if (wd.date.compareTo(nearestRainyDatum.date) < 0) {
+                    nearestRainyDatum = wd;
                 }
             }
         }
