29426728d758a7b98cf546e512a94158498ada94
노원희 (Wonhui Roh / Neo) <87654809+neoseurae12@users.noreply.github.com>
2024-09-14 18:57:28 +0900

    Title: 비오는 날, Time: 102ms, Memory: 9MB, Status: Passed - CodeTree

--- 240914/비오는 날/rainy-day.java
+++ 240914/비오는 날/rainy-day.java
@@ -17,7 +17,7 @@ class WeatherDatum {
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // todo
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         int n = Integer.parseInt(br.readLine());
