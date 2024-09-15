0d7a17561559c119d1657357e4d2c91aee0ba938
노원희 (Wonhui Roh / Neo) <87654809+neoseurae12@users.noreply.github.com>
2024-09-15 14:28:53 +0900

    Title: 앞에서부터 삭제하기 2, Time: 3076ms, Memory: 53MB, Status: Time Limit Exceed - CodeTree

--- 240915/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.java
+++ 240915/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.java
@@ -1,14 +1,18 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = sc.nextInt();
+        int n = Integer.parseInt(br.readLine());
         int[] nums = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            nums[i] = sc.nextInt();
+            nums[i] = Integer.parseInt(st.nextToken());
         }
 
         PriorityQueue<Integer> pq = new PriorityQueue<>();
