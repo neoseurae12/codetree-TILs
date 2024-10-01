50417eed67f4c55e0548a4e3a23856898866e3aa
노원희 (Wonhui Roh / Neo) <87654809+neoseurae12@users.noreply.github.com>
2024-10-01 13:08:09 +0900

    Title: 1이 3개 이상 있는 위치, Time: 270ms, Memory: 18MB, Status: Passed - CodeTree

---
+++ 241001/1이 3개 이상 있는 위치/place-more-than-3-ones.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static int n;
+    public static int[][] grid;
+
+    public static int[] dx = new int[]{1, 0, -1, 0};
+    public static int[] dy = new int[]{0, -1, 0, 1};
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+
+        grid = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                grid[i][j] = sc.nextInt();
+            }
+        }
+
+        int threeOrMore = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (check(i, j)) {
+                    threeOrMore++;
+                }
+            }
+        }
+
+        System.out.println(threeOrMore);
+    }
+
+    public static boolean check(int row, int column) {
+        int cnt = 0;
+
+        // 동 -> 남 -> 서 -> 북 체크
+        for (int dirNum = 0; dirNum < 4; dirNum++) {
+            int nx = row + dx[dirNum];
+            int ny = column + dy[dirNum];
+            if (inRange(nx, ny) && grid[nx][ny] == 1)
+                cnt++;
+        }
+
+        if (cnt >= 3)
+            return true;
+
+        return false;
+    }
+
+    public static boolean inRange(int x, int y) {
+        return (x >= 0 && x < n && y >= 0 && y < n);
+    }
+}
