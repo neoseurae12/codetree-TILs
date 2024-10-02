6953af2294960b62f44444f98ccd4616387484ea
노원희 (Wonhui Roh / Neo) <87654809+neoseurae12@users.noreply.github.com>
2024-10-02 20:14:23 +0900

    Title: 작은 구슬의 이동, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241002/작은 구슬의 이동/small-marble-movement.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class Main {
+
+    // n: 격자 n*n, t: t초 후
+    public static int n, t;
+    // (r, c): 초기 구슬의 위치, d: 초기 이동 방향
+    public static int r, c;
+    public static char d;
+
+    public static int[][] grid;
+
+    // 북 동 서 남
+    public static int[] dx = new int[]{0, 1, -1, 0};
+    public static int[] dy = new int[]{-1, 0, 0, 1};
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        t = sc.nextInt();
+
+        // -1 => for 인덱스 0부터 반영
+        r = sc.nextInt() - 1;
+        c = sc.nextInt() - 1;
+        // (0, 1)
+
+        d = sc.next().charAt(0);
+        int dNum = getNumOfD(d);
+
+        grid = new int[n][n];
+
+        while (t-- > 0) {
+            int nx = r + dx[dNum];
+            int ny = c + dy[dNum];
+
+            // 방향만 바꿀 경우
+            if (!inRange(nx, ny)) {
+                dNum = 3 - dNum;
+            }
+            // 방향대로 앞으로 나아갈 경우
+            else {
+                r= nx;
+                c = ny;
+            }
+        }
+
+        // +1 => for 인덱스 0부터 반영에서 원상태 복구
+        System.out.println(++r + " " + ++c);
+    }
+
+    public static boolean inRange(int x, int y) {
+        return (x >= 0 && x < n && y >= 0 && y < n);
+    }
+
+    public static int getNumOfD(char d) {
+        // 북 동 서 남
+        if (d == 'N')
+            return 0;
+        else if (d == 'E')
+            return 1;
+        else if (d == 'W')
+            return 2;
+        else
+            return 3;
+    }
+}
