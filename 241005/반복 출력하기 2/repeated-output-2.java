cfd57021626bd8a62e4b9b6fe4c04320995da536
노원희 (Wonhui Roh / Neo) <87654809+neoseurae12@users.noreply.github.com>
2024-10-05 16:02:04 +0900

    Title: 반복 출력하기 2, Time: 165ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241005/반복 출력하기 2/repeated-output-2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        printHelloWorld(N);
+    }
+
+    public static void printHelloWorld(int n) {
+        if (n == 0)
+            return;
+
+        printHelloWorld(n - 1);
+        System.out.println("HelloWorld");
+    }
+}
