f61e54ce35106fa3fe2e8b3520160e507d3bf270
노원희 (Wonhui Roh / Neo) <87654809+neoseurae12@users.noreply.github.com>
2024-12-05 14:22:40 +0900

    Title: 두 개의 격자 비교하기, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

--- 241205/두 개의 격자 비교하기/compare-two-grid.java
+++ 241205/두 개의 격자 비교하기/compare-two-grid.java
@@ -14,31 +14,21 @@ public class Main {
         arr2d_2 = new int[n][m];
         arr2d_new = new int[n][m];
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
                 arr2d_1[i][j] = sc.nextInt();
-            }
-        }
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
                 arr2d_2[i][j] = sc.nextInt();
-            }
-        }
+
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++)
+                arr2d_new[i][j] = arr2d_1[i][j] == arr2d_2[i][j] ? 0 : 1;
 
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (arr2d_1[i][j] == arr2d_2[i][j])
-                    arr2d_new[i][j] = 0;
-                else
-                    arr2d_new[i][j] = 1;
-            }
-        }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+            for (int j = 0; j < m; j++)
                 System.out.print(arr2d_new[i][j] + " ");
-            }
             System.out.println();
         }
     }
