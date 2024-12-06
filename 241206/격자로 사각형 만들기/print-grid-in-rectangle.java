3c3781c8efdd4e3a04cfbbf5d7dfbce3776258ca
노원희 (Wonhui Roh / Neo) <87654809+neoseurae12@users.noreply.github.com>
2024-12-06 23:00:26 +0900

    Title: 격자로 사각형 만들기, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241206/격자로 사각형 만들기/print-grid-in-rectangle.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        
+        int[][] grid = new int[n][n];
+
+        for (int i = 0; i < n; i++)
+            grid[0][i] = 1;
+            
+        for (int i = 0; i < n; i++)
+            grid[i][0] = 1;
+
+        for (int i = 1; i < n; i++)
+            for (int j = 1; j < n; j++)
+                grid[i][j] = grid[i][j-1] + grid[i-1][j] + grid[i-1][j-1];  // 왼쪽 + 위 + 왼쪽위
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++)
+                System.out.print(grid[i][j] + " ");
+            System.out.println();
+        }
+    }
+}
