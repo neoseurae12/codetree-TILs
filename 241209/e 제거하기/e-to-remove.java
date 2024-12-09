3b5aa8fbb562744ad7f760c8eacb6b1f238e6fac
노원희 (Wonhui Roh / Neo) <87654809+neoseurae12@users.noreply.github.com>
2024-12-09 18:15:33 +0900

    Title: e 제거하기, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241209/e 제거하기/e-to-remove.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+
+        String strWithoutE = str.replaceFirst("e", "");
+        System.out.println(strWithoutE);
+    }
+}
