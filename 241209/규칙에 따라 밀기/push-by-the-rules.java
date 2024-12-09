871f9c3655f649e2cfdb048c3bf7aa8a59c58bee
노원희 (Wonhui Roh / Neo) <87654809+neoseurae12@users.noreply.github.com>
2024-12-09 18:32:12 +0900

    Title: 규칙에 따라 밀기, Time: 129ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 241209/규칙에 따라 밀기/push-by-the-rules.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+        char[] commands = sc.next().toCharArray();
+        
+        int cntL = 0;
+        int cntR = 0;
+        for (int i = 0; i < commands.length; i++) {
+            if (commands[i] == 'L')
+                cntL++;
+            else if (commands[i] == 'R')
+                cntR++;
+        }
+        char resultDir = cntL > cntR ? 'L' : 'R';
+        int resultCnt = Math.abs(cntL - cntR);
+        
+        String result;
+        if (resultDir == 'L') {
+            String tail = str.substring(0, resultCnt);
+            String body = str.substring(resultCnt);
+            result = body + tail;
+        } else {    // 'R'
+            String head = str.substring(str.length() - resultCnt);
+            String body = str.substring(0, str.length() - resultCnt);
+            result = head + body;
+        }
+
+        System.out.println(result);
+    }
+}
