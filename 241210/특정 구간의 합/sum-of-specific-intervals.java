595e71931ce32fc7af99e8a40ff19adf0c3f77d9
노원희 (Wonhui Roh / Neo) <87654809+neoseurae12@users.noreply.github.com>
2024-12-10 17:26:26 +0900

    Title: 특정 구간의 합, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 241210/특정 구간의 합/sum-of-specific-intervals.java
+++ 241210/특정 구간의 합/sum-of-specific-intervals.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static int sumOfRange(int a1, int a2) {
         int sum = 0;
-        for (int i = a1 - 1; i < a2; i++) {
+        for (int i = a1; i <= a2; i++) {
             sum += arrA[i];
         }
 
@@ -18,9 +18,9 @@ public class Main {
         int n = sc.nextInt();
         int m = sc.nextInt();
 
-        arrA = new int[n];
+        arrA = new int[n+1];
 
-        for (int i = 0; i < n; i++)
+        for (int i = 1; i <= n; i++)
             arrA[i] = sc.nextInt();
 
         for (int i = 0; i < m; i++) {
