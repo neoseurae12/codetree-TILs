9646ddbde35bf690289d8c311ec219f7cffedf9d
노원희 (Wonhui Roh / Neo) <87654809+neoseurae12@users.noreply.github.com>
2024-12-16 00:53:34 +0900

    Title: 그 요일은, Time: 132ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241216/그 요일은/the-day-of-the-day.java
+++ 241216/그 요일은/the-day-of-the-day.java
@@ -2,11 +2,24 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class Main {
+    //                                          1   2   3   4   5   6   7   8   9  10  11  12
+    public static int[] daysOfMonth = new int[]{0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    public static int getDays(int m, int d) {
+        int days = 0;
+
+        for (int i = 1; i < m; i++) {
+            days += daysOfMonth[i];
+        }
+        days += d;
+
+        return days;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        //                                1   2   3   4   5   6   7   8   9  10  11  12
-        int[] daysOfMonth = new int[]{0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        
         String[] daysOfWeek = new String[]{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
         
         int m1, d1, m2, d2;
@@ -25,28 +38,14 @@ public class Main {
         // 4. 2 31 -> 3 2
         // 5. 3 9
 
-        int m = m1;
-        int d = d1 + Arrays.asList(daysOfWeek).indexOf(day);
-        if (d > daysOfMonth[m]) {
-            d %= daysOfMonth[m];
-            m++;
-        }
         int count = 0;
 
-        while (m < m2 || d <= d2) {
+        int days1 = getDays(m1, d1);
+        int days2 = getDays(m2, d2);
+        int total = days2 - days1;
+        count = total / 7;
+        if (total % 7 >= Arrays.asList().indexOf(day))
             count++;
-
-            //System.out.println(m + " " + d);
-
-            d += 7;
-            if (d > daysOfMonth[m]) {
-                d %= daysOfMonth[m];
-                m++;
-            }
-
-            if (m > 12)
-                break;
-        }
 
         System.out.println(count);
     }
