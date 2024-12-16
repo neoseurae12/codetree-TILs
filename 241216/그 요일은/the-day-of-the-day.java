72b999d6587816be221d7d44d2cab5eff5c9a423
노원희 (Wonhui Roh / Neo) <87654809+neoseurae12@users.noreply.github.com>
2024-12-16 21:55:39 +0900

    Title: 그 요일은, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 241216/그 요일은/the-day-of-the-day.java
+++ 241216/그 요일은/the-day-of-the-day.java
@@ -2,10 +2,12 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class Main {
+    public static int answer;
     //                                          1   2   3   4   5   6   7   8   9  10  11  12
     public static int[] daysOfMonth = new int[]{0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    public static String[] daysOfWeek = new String[]{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
 
-    public static int getDays(int m, int d) {
+    public static int numOfDays(int m, int d) {
         int days = 0;
 
         for (int i = 1; i < m; i++) {
@@ -19,8 +21,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String[] daysOfWeek = new String[]{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
-        
         int m1, d1, m2, d2;
         String day;
 
@@ -30,22 +30,16 @@ public class Main {
         d2 = sc.nextInt();
         day = sc.next();
 
-        // 2 5
-        // 1. 2 10
-        // 2. 2 17
-        // 3. 2 24
-        // 4. 2 31 -> 3 2
-        // 5. 3 9
+        int days1 = numOfDays(m1, d1);
+        int days2 = numOfDays(m2, d2);
+        int currentDay = Arrays.asList(daysOfWeek).indexOf("Mon");
+        int targetDay = Arrays.asList(daysOfWeek).indexOf(day);
+        
+        for (int date = days1; date <= days2; date++) {
+            if (currentDay == targetDay) answer++;
+            currentDay = (currentDay + 1) % 7;
+        }
 
-        int count = 0;
-
-        int days1 = getDays(m1, d1);
-        int days2 = getDays(m2, d2);
-        int total = days2 - days1;
-        count = total / 7;
-        if (total % 7 >= Arrays.asList(daysOfWeek).indexOf(day))
-            count++;
-
-        System.out.println(count);
+        System.out.println(answer);
     }
 }
