d6676aec7d0fac907d059b4695e827de4542ed3b
노원희 (Wonhui Roh / Neo) <87654809+neoseurae12@users.noreply.github.com>
2024-12-18 23:00:17 +0900

    Title: 2진수로 변환하기, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241218/2진수로 변환하기/convert-to-binary.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int[] digits = new int[20];
+        int index = 0;
+
+        while (true) {
+            if (n < 2) {
+                digits[index] = n % 2;
+                break;
+            }
+
+            digits[index] = n % 2;
+            n /= 2;
+            index++;
+        }
+
+        for (int i = index; i >= 0; i--) {
+            System.out.print(digits[i]);
+        }
+    }
+}
