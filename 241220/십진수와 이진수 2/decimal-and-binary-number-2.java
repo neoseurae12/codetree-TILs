2eb978567e5cfc108081840ce8d4a8acd41a5b52
노원희 (Wonhui Roh / Neo) <87654809+neoseurae12@users.noreply.github.com>
2024-12-20 00:51:26 +0900

    Title: 십진수와 이진수 2, Time: 117ms, Memory: 10MB, Status: Passed - CodeTree

--- 241220/십진수와 이진수 2/decimal-and-binary-number-2.java
+++ 241220/십진수와 이진수 2/decimal-and-binary-number-2.java
@@ -2,11 +2,12 @@ import java.util.Scanner;
 import java.lang.StringBuilder;
 
 public class Main {
-    public static int toDecimal(int binary) {
-        String strB = Integer.toString(binary);
+    public static final int MAX_DIGIT = 20;
+
+    public static int toDecimal(String binary) {
         int decimal = 0;
-        for (int i = 0; i < strB.length(); i++) {
-            int b = (int)strB.charAt(i) - '0';
+        for (int i = 0; i < binary.length(); i++) {
+            int b = (int)binary.charAt(i) - '0';
             decimal = 2 * decimal + b;
         }
 
@@ -14,7 +15,7 @@ public class Main {
     }
 
     public static String toBinary(int decimal) {
-        int[] digits = new int[20];
+        int[] digits = new int[MAX_DIGIT];
         int cnt = 0;
 
         while (true) {
@@ -38,7 +39,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
+        String n = sc.next();
 
         // 이진수 -> 십진수
         int d = toDecimal(n);
