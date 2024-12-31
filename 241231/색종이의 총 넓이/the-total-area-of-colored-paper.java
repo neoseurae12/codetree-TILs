749ec8267aa263d20cbd001fef00ea6d83995e91
노원희 (Wonhui Roh / Neo) <87654809+neoseurae12@users.noreply.github.com>
2024-12-31 12:05:25 +0900

    Title: 색종이의 총 넓이, Time: 156ms, Memory: 10MB, Status: Passed - CodeTree

--- 241231/색종이의 총 넓이/the-total-area-of-colored-paper.java
+++ 241231/색종이의 총 넓이/the-total-area-of-colored-paper.java
@@ -4,10 +4,11 @@ public class Main {
     public static final int PAPER_SIZE = 8;
     public static final int OFFSET = 100;
     public static final int MAX_R = 100 * 2 + 1;
+    public static final int MAX_N = 100;
 
     public static int n;
     public static int[][] grid = new int[MAX_R][MAX_R];
-    public static int[][] squares = new int[100][2];
+    public static int[][] squares = new int[MAX_N][2];
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -19,22 +20,17 @@ public class Main {
             squares[i][1] = sc.nextInt() + OFFSET;
         }
 
-        for (int i = 0; i < n; i++) {
-            for (int j = squares[i][0]; j < squares[i][0] + PAPER_SIZE; j++) {
-                for (int k = squares[i][1]; k < squares[i][1] + PAPER_SIZE; k++) {
+        for (int i = 0; i < n; i++)
+            for (int j = squares[i][0]; j < squares[i][0] + PAPER_SIZE; j++)
+                for (int k = squares[i][1]; k < squares[i][1] + PAPER_SIZE; k++)
                     grid[j][k]++;
-                }
-            }
-        }
 
         int area = 0;
 
-        for (int i = 0; i < MAX_R; i++) {
-            for (int j = 0; j < MAX_R; j++) {
+        for (int i = 0; i < MAX_R; i++)
+            for (int j = 0; j < MAX_R; j++)
                 if (grid[i][j] > 0)
                     area++;
-            }
-        }
 
         System.out.println(area);
     }
