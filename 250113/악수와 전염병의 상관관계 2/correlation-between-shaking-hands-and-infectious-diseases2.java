2b08ac39364e6b9a389880bf5dbedddba509a6dc
노원희 (Wonhui Roh / Neo) <87654809+neoseurae12@users.noreply.github.com>
2025-01-13 13:01:21 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 250113/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.java
+++ 250113/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.java
@@ -8,6 +8,11 @@ public class Main {
     public static int[] handshakes = new int[MAX_t + 1];
     // int[] infections: handshake에서 K번째까지의 개발자들 '감염' 처리
     public static int[] infections = new int[MAX_N + 1];
+    // int[] k: 각 개발자의 옮길 수 있는 횟수 기록
+    public static int[] k = new int[MAX_N + 1];
+
+    public static int[] x = new int[MAX_t + 1];
+    public static int[] y = new int[MAX_t + 1];
 
     // N: 개발자 수, K: 옮길 수 있는 횟수, P: 처음 전염병 걸린 개발자의 번호, T: 총 악수 횟수
     public static int N, K, P, T;
@@ -20,29 +25,40 @@ public class Main {
         P = sc.nextInt();
         T = sc.nextInt();
 
+        // 악수 기록
         for (int i = 0; i < T; i++) {
             // t: 악수 나눈 시점, x, y: 서로 악수한 개발자들의 번호
             int t = sc.nextInt();
-            int x = sc.nextInt();
-            int y = sc.nextInt();
-
-            if (x == P || y == P) {
-                // P와 악수를 나눈 상대 개발자의 번호
-                int partner = x == P ? y : x;
-
-                handshakes[t] = partner;
-            }
+            x[t] = sc.nextInt();
+            y[t] = sc.nextInt();
         }
         
         // 초기 감염자
         infections[P] = 1;
+        // 시간 흐름대로 추적
         for (int i = 1; i <= MAX_t; i++) {
-            int partner = handshakes[i];
-            // i초에서 악수를 나눈 상대가 있고 & 아직 전염시킬 횟수 남아있다면 -> '전염' 처리
-            if (partner != 0 && K > 0) {
-                infections[partner] = 1;
-                K--;
+            // 전염된 사람끼리 만나도 전염병을 옮기게 되는 횟수에 포함
+            if (infections[x[i]] == 1 && infections[y[i]] == 1) {
+                k[x[i]]++;
+                k[y[i]]++;
             }
+            // x가 감염자인 경우
+            else if (infections[x[i]] == 1) {
+                // 감염시킬 수 있다면 -> y도 감염
+                if (k[x[i]] < K) {
+                    infections[y[i]] = 1;
+                    k[x[i]]++;
+                }
+            }
+            // y가 감염자인 경우
+            else if (infections[y[i]] == 1) {
+                // 감염시킬 수 있다면 -> x도 감염
+                if (k[y[i]] < K) {
+                    infections[x[i]] = 1;
+                    k[y[i]]++;
+                }
+            }
+            // 둘 다 감염자가 아니라면 -> pass
         }
         
         for (int i = 1; i <= N; i++) {
