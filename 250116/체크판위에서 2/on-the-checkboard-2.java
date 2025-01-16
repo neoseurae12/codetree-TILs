f661ca34cd9b60dc3a6b2afce7347f9d8425bd17
노원희 (Wonhui Roh / Neo) <87654809+neoseurae12@users.noreply.github.com>
2025-01-16 20:08:13 +0900

    Title: 체크판위에서 2, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

--- 250116/체크판위에서 2/on-the-checkboard-2.java
+++ 250116/체크판위에서 2/on-the-checkboard-2.java
@@ -19,21 +19,36 @@ public class Main {
                 grid[i][j] = sc.next().charAt(0);
 
         // 아래의 조건들을 만족하는 경우의 수 구하기
-            // 시작 -> A -> B -> 끝
+            // 1 -> 2 -> 3 -> 4
             // 점프할 때마다 색이 달라야 한다
             // 한 칸 이상 오른쪽, 한 칸 이상 아래쪽
         int ans = 0;
-        char currColor = grid[0][0];
+        // 1: grid[0][0]
+        // 2:
         for (int i = 1; i < r; i++) {
             for (int j = 1; j < c; j++) {
-                if (grid[i][j] == currColor)
+                // 1과 같은 색인 경우 -> continue
+                if (grid[i][j] == grid[0][0])
                     continue;
 
-                currColor = grid[i][j];
-                for (int m = i + 1; m < r - 1; m++) {
-                    for (int n = j + 1; n < c - 1; n++) {
-                        if (grid[m][n] != currColor)
-                            ans++;
+                // 3:
+                for (int m = i + 1; m < r; m++) {
+                    for (int n = j + 1; n < c; n++) {
+                        // 2와 같은 색 -> continue
+                        if (grid[m][n] == grid[i][j])
+                            continue;
+                        
+                        // 4: grid[r-1][c-1]
+                        for (int f = m + 1; f < r; f++) {
+                            for (int g = n + 1; g < c; g++) {
+                                // 3과 같은 색 -> continue
+                                if (grid[f][g] == grid[m][n])
+                                    continue;
+                                
+                                if (f == r - 1 && g == c - 1)
+                                    ans++;
+                            }
+                        }
                     }
                 }
             }
