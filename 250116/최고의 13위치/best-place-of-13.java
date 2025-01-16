14c8aa84df03fee68b3061e6d11990a12cb5d70a
노원희 (Wonhui Roh / Neo) <87654809+neoseurae12@users.noreply.github.com>
2025-01-16 16:00:05 +0900

    Title: 최고의 13위치, Time: 138ms, Memory: 11MB, Status: Passed - CodeTree

--- 250116/최고의 13위치/best-place-of-13.java
+++ 250116/최고의 13위치/best-place-of-13.java
@@ -13,22 +13,19 @@ public class Main {
         // 입력: n
         n = sc.nextInt();
         // 입력: 격자 정보
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
                 grid[i][j] = sc.nextInt();
-            }
-        }
 
         // 1 * 3 격자의 최대 동전의 개수 구하기 by 완전탐색
         int maxCoin = 0;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
             for (int j = 0; j <= n - SUB_GRID; j++) {
                 int numCoin = 0;
                 for (int k = 0; k < SUB_GRID; k++)
                     numCoin += grid[i][j + k];
                 maxCoin = Math.max(maxCoin, numCoin);
             }
-        }
 
         // 출력: 최대 동전의 개수
         System.out.println(maxCoin);
