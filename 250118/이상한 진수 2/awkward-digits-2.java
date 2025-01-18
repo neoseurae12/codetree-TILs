187f18947fd2dfeed8ed189c3e11e89c5f9f3092
노원희 (Wonhui Roh / Neo) <87654809+neoseurae12@users.noreply.github.com>
2025-01-18 22:46:16 +0900

    Title: 이상한 진수 2, Time: 117ms, Memory: 10MB, Status: Passed - CodeTree

--- 250118/이상한 진수 2/awkward-digits-2.java
+++ 250118/이상한 진수 2/awkward-digits-2.java
@@ -1,10 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
-    public static final int MAX_LEN_A = 10;
-
     public static String a;
-    public static int n = 1;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -12,40 +9,23 @@ public class Main {
         // 입력: 문자열 a
         a = sc.next();
 
-        // 문자열 a의 10진법 값 구하기
-        for (int i = 1; i < a.length(); i++) {
-            n = 2 * n + ((int)a.charAt(i) - '0');
-        }
-
         // 완전 탐색: 한 자리씩 바꿔보면서 본래 a 값에 반영 -> 최댓값 체크
         int maxN = 0;
-        for (int i = 1; i < a.length(); i++) {
-            // 자릿수의 십진수
-            int digit = 1;
-            for (int j = 0; j < a.length() - i - 1; j++) {
-                digit *= 2;
-            }
+        for (int i = 0; i < a.length(); i++) {
+            // i번째 자릿수 변경
+            a = a.substring(0, i) + (char)('0' + '1' - a.charAt(i)) + a.substring(i + 1);
 
-            int digitNum = (int) a.charAt(i) - '0';
-            if (digitNum == 0) {
-                // 자릿수의 십진수 더하기
-                n += digit;
-            }
-            else {    // digitNum == 1
-                // 자릿수의 십진수 빼기
-                n -= digit;
+            // 십진수로 변환
+            int n = 0;
+            for (int j = 0; j < a.length(); j++) {
+                n = 2 * n + ((int)a.charAt(j) - '0');
             }
 
             // 최댓값과 구한값 비교 -> 최댓값 갱신
             maxN = Math.max(maxN, n);
 
-            // 더하거나 뺐던 값 다시 되돌리기
-            if (digitNum == 0) {
-                n -= digit;
-            }
-            else {
-                n += digit;
-            }
+            // i번째 자릿수 되돌리기
+            a = a.substring(0, i) + (char)('0' + '1' - a.charAt(i)) + a.substring(i + 1);
         }
 
         // 출력: 최댓값
