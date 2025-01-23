05a3fbf6347302faef18d1b6aad32133fa7d0d98
노원희 (Wonhui Roh / Neo) <87654809+neoseurae12@users.noreply.github.com>
2025-01-23 15:22:36 +0900

    Title: 황금비율 토스트, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250123/황금비율 토스트/golden-toast.java
+++ 250123/황금비율 토스트/golden-toast.java
@@ -11,53 +11,46 @@ public class Main {
 
         n = sc.nextInt();
         m = sc.nextInt();
-
         str = sc.next();
 
         LinkedList<Character> l = new LinkedList<>();
-        for (int i = 0; i < str.length(); i++) {
+        for (int i = 0; i < str.length(); i++)
             l.add(str.charAt(i));
-        }
         // c - o - d - e - t
 
         // 처음 위치: 맨 마지막
         ListIterator<Character> it = l.listIterator(l.size());
-        for (int i = 0; i < m; i++) {
+        while (m-- > 0) {
             // 입력: 명령어
             char command = sc.next().charAt(0);
 
             // Case 1: L
                 // 만약 앞쪽으로 갈 수 있다면 -> 앞쪽으로 위치 변경
-            if (command == 'L') {
+            if (command == 'L')
                 if (it.hasPrevious())
                     it.previous();
-            }
             // Case 2: R
                 // 만약 뒷쪽으로 갈 수 있다면 -> 뒷쪽으로 위치 변경
-            else if (command == 'R') {
+            else if (command == 'R')
                 if (it.hasNext())
                     it.next();
-            }
             // Case 3: D
                 // 만약 뒷쪽으로 갈 수 있다면 -> 뒷쪽으로 위치 변경 + 제거
-            else if (command == 'D') {
+            else if (command == 'D')
                 if (it.hasNext()) {
                     it.next();
                     it.remove();
                 }
-            }
             // Case 4: P &
                 // & 문자 추가
             else if (command == 'P') {
                 char e = sc.next().charAt(0);
-
                 it.add(e);
             }
         }
         
         it = l.listIterator();
-        for (int i = 0; i < l.size(); i++) {
+        while (it.hasNext())
             System.out.print(it.next());
-        }
     }
 }
