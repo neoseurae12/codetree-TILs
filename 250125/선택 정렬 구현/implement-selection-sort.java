50d856ff1467fc52846c3172ed86d8ff480005b8
노원희 (Wonhui Roh / Neo) <87654809+neoseurae12@users.noreply.github.com>
2025-01-25 15:11:24 +0900

    Title: 선택 정렬 구현, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

--- 250125/선택 정렬 구현/implement-selection-sort.java
+++ 250125/선택 정렬 구현/implement-selection-sort.java
@@ -6,35 +6,36 @@ public class Main {
     public static int n;
     public static int[] numbers = new int[MAX_N];
 
+    public static void selectionSort() {
+        // 루프 1: 최솟값을 채울 자리 선택
+        for (int i = 0; i < n - 1; i++) {
+            // 루프 2: 루프 1 내 최솟값을 찾음
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++)
+                if (numbers[j] < numbers[minIndex])
+                    minIndex = j;
+
+            // 루프 1: 최솟값을 채울 자리와 최솟값의 현재 자리랑 바꾸기
+            int tmp = numbers[i];
+            numbers[i] = numbers[minIndex];
+            numbers[minIndex] = tmp;
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
         // 입력: n
         n = sc.nextInt();
         // 입력: 정렬 전의 원소값들
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
             numbers[i] = sc.nextInt();
-        }
 
         // 선택 정렬
-            // 루프 1: 최솟값을 채울 자리 선택
-            // 루프 2: 루프 1 내 최솟값을 찾음
-            // 루프 1: 최솟값을 채울 자리와 최솟값의 현재 자리랑 바꾸기
-        for (int i = 0; i < n - 1; i++) {
-            int minIndex = i;
-            for (int j = i + 1; j < n; j++) {
-                if (numbers[j] < numbers[minIndex]) {
-                    minIndex = j;
-                }
-            }
-            int tmp = numbers[i];
-            numbers[i] = numbers[minIndex];
-            numbers[minIndex] = tmp;
-        }
+        selectionSort();
 
         // 출력: 정렬 후의 원소값들
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
             System.out.print(numbers[i] + " ");
-        }
     }
 }
