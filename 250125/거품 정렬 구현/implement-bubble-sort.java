403479940332a2cc06cc602e53749b8908aa3777
노원희 (Wonhui Roh / Neo) <87654809+neoseurae12@users.noreply.github.com>
2025-01-25 14:37:27 +0900

    Title: 거품 정렬 구현, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

--- 250125/거품 정렬 구현/implement-bubble-sort.java
+++ 250125/거품 정렬 구현/implement-bubble-sort.java
@@ -13,24 +13,20 @@ public class Main {
         n = sc.nextInt();
 
         // 입력: 정렬 전의 원소값들
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
             numbers[i] = sc.nextInt();
-        }
 
         // 비교 -> 정렬
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - 1 - i; j++) {
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - 1 - i; j++)
                 if (numbers[j] > numbers[j + 1]) {
                     int tmp = numbers[j];
                     numbers[j] = numbers[j+1];
                     numbers[j+1] = tmp;
                 }
-            }
-        }
         
         // 출력: 정렬 후의 원소값들
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
             System.out.print(numbers[i] + " ");
-        }
     }
 }
